ba13ee07fdcb06a8abb121126cdc6c47f7423adc
bbrissette96 <beaubrissette@gmail.com>
2017-04-03 22:54:54 -0400

    Update errors.html.php

--- Lab1/Templates/errors.html.php
+++ Lab1/Templates/errors.html.php
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 <?php if ( isset($errors) && is_array($errors) ) : ?>
     <ul>
         <?php foreach ($errors as $error): ?>
             <li class="bg-danger"><?php echo $error; ?></li>
         <?php endforeach; ?>
     </ul>
-=======
-<?php if ( isset($errors) && is_array($errors) ) : ?>
-    <ul>
-        <?php foreach ($errors as $error): ?>
-            <li class="bg-danger"><?php echo $error; ?></li>
-        <?php endforeach; ?>
-    </ul>
->>>>>>> origin/master
 <?php endif; ?>
