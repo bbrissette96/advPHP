eb130dadf5ed344aa41336af5c79c8dc9370e559
bbrissette96 <beaubrissette@gmail.com>
2017-04-03 22:54:34 -0400

    Update success.html.php

--- Lab1/Templates/success.html.php
+++ Lab1/Templates/success.html.php
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
 <?php if ( isset($msgSuccess) && strlen($msgSuccess) ) : ?>
     <p class="alert alert-success"><?php echo $msgSuccess; ?></p>
-=======
-<?php if ( isset($msgSuccess) && strlen($msgSuccess) ) : ?>
-    <p class="alert alert-success"><?php echo $msgSuccess; ?></p>
->>>>>>> origin/master
 <?php endif; ?>
