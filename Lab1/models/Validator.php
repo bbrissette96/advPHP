bede30d921ab91552acdb0a2e99d26f92dc24ec1
bbrissette96 <beaubrissette@gmail.com>
2017-04-03 22:56:26 -0400

    Update Validator.php

--- Lab1/models/Validator.php
+++ Lab1/models/Validator.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 
@@ -22,29 +21,4 @@ function isValidDate ($date){
     return (bool)strtotime($date);
 }
 
-=======
-<?php
-
-
-function isValidEmail($email){
-    if (filter_var($email, FILTER_VALIDATE_EMAIL) !== false){
-        return true;
-    }
-    return false;
-}
-
-function isValidZIP($zip){
-    $zipRegex = '/^[0-9]{5}$/';
-
-    if( !preg_match($zipRegex, $zip) ){
-        return false;
-    }
-    return true;
-}
-
-function isValidDate ($date){
-    return (bool)strtotime($date);
-}
-
->>>>>>> origin/master
 ?>
